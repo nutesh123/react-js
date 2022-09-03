c98bb97c4c8d8d7be5a45a5daff976b4dfe7315a
harry <nuteshgorde@gmail.com>
2022-09-03 09:11:03 +0530

    sms task 9

---
+++ src/componenets/Expenseitem.js
@@ -0,0 +1,16 @@
+
+
+
+function Expenseitem(props){
+
+
+    return (
+        <div>
+        <p>{props.title}</p>
+        <p>{props.id}</p>
+        <h1> {props.place}</h1>
+        </div>
+    )
+}
+
+export default Expenseitem;
---
+++ src/componenets/Expenseplace.js
@@ -0,0 +1,14 @@
+
+
+function Expenseplace (properties){
+
+    return(
+
+        <div>
+            
+            <h4>{properties.place}</h4>
+            </div>
+    )
+}
+
+export default Expenseplace ;
